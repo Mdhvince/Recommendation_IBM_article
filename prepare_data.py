e4bf4f7652e9badc7fd1cca55caa77d65f238612
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2019-01-16 14:28:48 +0100

    feat: clean pipeline
    The method to clean the data was added

---
+++ prepare_data.py
@@ -0,0 +1,72 @@
+import pandas as pd 
+import numpy as np 
+
+def clean_pipeline(df_reviews, df_items):
+    
+    df = df_reviews.copy()
+    df_content = df_items.copy()
+    
+    if 'Unnamed: 0' in df.columns:
+        del df['Unnamed: 0']
+        
+    if 'Unnamed: 0' in df_content.columns:
+        del df_content['Unnamed: 0']
+
+    # make sure all articles from df are in df_content
+    df = pd.merge(df, df_content[['article_id']], on='article_id', how='inner')
+
+    df_content = df_content.drop(labels='doc_status', axis=1)
+    df_content = df_content.drop_duplicates()
+
+    #df = df.replace('no_email', np.nan)
+
+    def email_mapper():
+        coded_dict = dict()
+        cter = 1
+        email_encoded = []
+
+        for val in df['email']:
+            if val not in coded_dict:
+                coded_dict[val] = cter
+                cter+=1
+
+            email_encoded.append(coded_dict[val])
+        return email_encoded
+
+    email_encoded = email_mapper()
+    del df['email']
+    df['user_id'] = email_encoded
+
+    # Rename my article name field to be the same in both dfs
+    df_content['title'] = df_content['doc_full_name']
+    df_content = df_content.drop(labels='doc_full_name', axis=1)
+
+    df.article_id = df.article_id.astype('int64')
+    df_content.article_id = df_content.article_id.astype('int64')
+
+    # number of times the user seen the article
+    pre_data = dict(df.groupby(['user_id', 'article_id'])['article_id'].count())
+
+    list_user = []
+    list_article_id = []
+    list_nb_interactions = []
+
+    for key, val in pre_data.items():
+        list_user.append(key[0])
+        list_article_id.append(key[1])
+        list_nb_interactions.append(val)
+
+    zipped_list = list(zip(list_user, list_article_id, list_nb_interactions))
+    interaction_user = pd.DataFrame(zipped_list, columns=['user_id','article_id','nb_interactions_user_article'])
+
+    df = pd.merge(df, interaction_user, on=['user_id','article_id'])
+
+    # The nb interactions was shown implicitly with the number of rows
+    # Now I have information about the number of interactions by creating this column
+    # I can drop duplicated rows
+    df = df.drop_duplicates(keep='first')
+    df = df.reset_index().drop(labels='index', axis=1)
+
+    df['date'] = 0
+    
+    return df, df_content
