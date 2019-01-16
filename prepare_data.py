a8d6d279d422b45881bd94c856451334d91d95a0
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2019-01-16 15:29:23 +0100

    load all necessary datas for inference

--- prepare_data.py
+++ prepare_data.py
@@ -2,26 +2,15 @@ import pandas as pd
 import numpy as np
 import recommender as r
 
-# Nlp
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-# Prepare data
-from prepare_data_functions import tokenize
-
-#df = pd.read_csv('user-item-interactions.csv')
-#df_content = pd.read_csv('articles_community.csv')
-
-# Cleanning Pipeline
-#df, df_content = clean_pipeline(df_reviews=df, df_items=df_content)
-
-# Prepare tfidf matrix
-tfidf = TfidfVectorizer(tokenizer=tokenize, ngram_range=(1, 2))
-df_content['doc_description'] = df_content['doc_description'].fillna('')
-tfidf_matrix = tfidf.fit_transform(df_content['doc_description'])
+# Load all necessay datas
+df = pd.read_csv('df_clean.csv').iloc[:,1:]
+df_content = pd.read_csv('df_content_clean.csv').iloc[:,1:]
+tfidf_matrix = np.load('tfidf_matrix.npy')
 
 # Create User-Item df
 #user_item_df = create_user_item(df=df)
 
+# Load rec object
 from sklearn.externals import joblib
 rec = joblib.load("rec_sys.dat")
 
