88ecdbadddf6f9874fd5bd895787e9621ec082b2
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2019-01-16 15:38:37 +0100

    save dot prod matrix for inference

--- production_ready.py
+++ production_ready.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import recommender as r
 
-from prep_data_display import *
+from prep_data_display import display_recommendations
 
 # Load all necessay datas
 df = pd.read_csv('df_clean.csv').iloc[:,1:]
@@ -23,34 +23,6 @@ def prep_get_similar_user():
 
 dot_product_matrix_user = prep_get_similar_user()
 
-
-def display_recommendations(rec_ids, rec_names, message, rec_ids_users,
-                            rec_user_articles):
-    
-    if type(rec_ids) == type(None):
-        print(f"{message}")
-    
-    else:
-        dict_id_name = dict(zip(rec_ids, rec_names))
-        
-        if type(rec_ids_users) != type(None):
-            print('Matrix Factorisation SVD:')
-            print(f"\t{message}")
-            
-            for key, val  in dict_id_name.items():
-                print(f"\t- ID items: {key}")
-                print(f"\tName: {val}\n")
-
-            print('CF User Based:')
-            print('\tUser that are similar to you also seen:\n')
-            for i in rec_user_articles[:5]:
-                print(f"\t- {i}")
-        else:
-            print(f"\t{message}")
-            dict_id_name = dict(zip(rec_ids, rec_names))
-            for key, val  in dict_id_name.items():
-                print(f"\t- ID items: {key}")
-                print(f"\tName: {val}\n")
 
 
 rec_ids, rec_names, message, rec_ids_users, rec_user_articles = (
